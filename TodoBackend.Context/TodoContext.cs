1737aa3c438aeddc6a9c75d21b7f4e3201d3abed
Goldenstriker <ndparab240@gmail.com>
2023-12-09 09:23:02 +0530

    added logic to update datecreate and datemodified on every update

--- TodoBackend.Context/TodoContext.cs
+++ TodoBackend.Context/TodoContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TodoBackend.Context.Models;
 
 namespace TodoBackend.Context
@@ -12,5 +13,26 @@ namespace TodoBackend.Context
         internal DbSet<Todo> Todos { get; set; }
 
         internal DbSet<TodoItem> TodoItems { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            IEnumerable<EntityEntry<IBaseModel>> entityEntries = ChangeTracker.Entries<IBaseModel>();
+
+            foreach (EntityEntry<IBaseModel> entityEntry in entityEntries)
+            {
+                switch (entityEntry.State)
+                {
+                    case EntityState.Added:
+                        entityEntry.Entity.DateCreated = DateTime.UtcNow;
+                        entityEntry.Entity.DateModified = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entityEntry.Entity.DateModified = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
